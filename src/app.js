071ec5853b4fef30bf464eba2288482d2ad1be6e
Julián David Pineda Betancurth <89669806+juliandavidpineda@users.noreply.github.com>
2025-05-31 04:01:57 +0000

    Inicio

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let quien = ["Jesús", "el enemigo", "Tu angel de la guarda", "La intecesión"];
+  let accion = [];
+  let que = [];
+  let cuando = [];
+
+  let excusa = quien[Math.floor(Math.random() * quien.length )] + " " + 
+
   console.log("Hello Rigo from the console!");
 };
