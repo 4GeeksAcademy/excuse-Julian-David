5063b242824bdc8347bec4bca85d3bc1a93073e8
Julián David Pineda Betancurth <89669806+juliandavidpineda@users.noreply.github.com>
2025-05-31 04:17:29 +0000

    Inicio.2

--- src/app.js
+++ src/app.js
@@ -6,12 +6,12 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let quien = ["Jesús", "el enemigo", "Tu angel de la guarda", "La intecesión"];
-  let accion = [];
-  let que = [];
-  let cuando = [];
+  let quien = ["Jesús", "El enemigo", "Tu angel de la guarda", "La intercesión"];
+  let accion = ["te salvará", "te enseñará", "lo permitirá", "te dirá que no"];
+  let que = ["los secretos", "del pecado", "tus anhelos", "la verdad"];
+  let cuando = ["en tu juicio final", "en el momento de tu muerte", "en medio de la tentación", "en la tribulación"];
 
-  let excusa = quien[Math.floor(Math.random() * quien.length )] + " " + 
+  let excusa = quien[Math.floor(Math.random() * quien.length )] + " " + accion[Math.floor(Math.random()) * accion.length] + " " + que[Math.floor(Math.random() * que.length)] + " " + cuando[Math.floor(Math.random() * cuando.length)];
 
-  console.log("Hello Rigo from the console!");
+  console.log(excusa);
 };
