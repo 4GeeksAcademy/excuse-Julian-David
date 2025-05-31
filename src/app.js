e23b52ee14ed62d8d0a84950b10cabbb7572191e
Julián David Pineda Betancurth <89669806+juliandavidpineda@users.noreply.github.com>
2025-05-31 18:48:05 +0000

    Avance final

--- src/app.js
+++ src/app.js
@@ -7,11 +7,15 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   let quien = ["Jesús", "El enemigo", "Tu angel de la guarda", "La intercesión"];
-  let accion = ["te salvará", "te enseñará", "lo permitirá", "te dirá que no"];
-  let que = ["los secretos", "del pecado", "tus anhelos", "la verdad"];
+  let accion = ["te salvará,", "te enseñará", "lo permitirá", "te dirá que no"];
+  let que = ["los secretos", "del pecado", "tus anhelos"];
   let cuando = ["en tu juicio final", "en el momento de tu muerte", "en medio de la tentación", "en la tribulación"];
 
   let excusa = quien[Math.floor(Math.random() * quien.length )] + " " + accion[Math.floor(Math.random()) * accion.length] + " " + que[Math.floor(Math.random() * que.length)] + " " + cuando[Math.floor(Math.random() * cuando.length)];
 
-  console.log(excusa);
+      document.getElementById("excuse").textContent = excusa;
+
 };
+  
+
+
